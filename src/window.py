a45f4c648d326f00710e2bdf135d29fccd1bfb5d
Cgun-art <migueloproio903@gmail.com>
2025-11-16 17:18:11 -0300

    Update window.py

--- src/window.py
+++ src/window.py
@@ -1,1 +1,14 @@
- 
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title("Amoon (Xsinq)")
+root.geometry("500x700")
+
+root.mainloop()
+
+import root.title
+import root.geometry
+import root.mainloop
+import root
+import tk
